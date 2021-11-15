0aa9c73c65b5c7c824dbdf5dd8ade050c772a6f4
Jeff Cela <cela.j@labrador-company.com>
2021-11-14 20:51:45 -0500

    duplicated the files from Project 1, intial commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Desktop Navigation script
+
+$(document).ready(function(){
+   $('.modal').modal();
+   $('.dropdown-trigger').dropdown();
+});
+
+// Mobile Navigation Script
+
+$(document).ready(function(){
+$('.sidenav').sidenav();
+});
+
+$(`.modalselector`).leanModal({
+      dismissible: false
+});
